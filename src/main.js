39fba28b65a2ee15a6f81cf6a461bd5dc6c25e1e
Leonardo Sauzza <leonardo.sauzza@globallogic.com>
2023-06-22 19:59:44 -0300

    pokemon sprite working

--- src/main.js
+++ src/main.js
@@ -1,17 +1,464 @@
 import kaboom from "kaboom"
 
-const k = kaboom(
-	{
-		scale: 4,
-		background: [0,0,0]
+// const k = kaboom(
+// 	{
+// 		scale: 4,
+// 		background: [0, 0, 0]
+// 	}
+// )
+
+// k.loadSprite("bean", "sprites/bean.png")
+// k.loadSpriteAtlas("sprites/001.png", {
+// 	"bul": {
+// 		x: 0,
+// 		y: 0,
+// 		width: 64,
+// 		height: 64,
+// 		sliceX: 4,
+// 		sliceY: 4,
+// 	}
+// })
+
+// const player = map.spawn([
+// 	sprite("bul"),
+// 	area({ shape: new Rect(vec2(0, 6), 12, 12) }),
+// 	body(),
+// 	anchor("center"),
+// 	tile()
+// ], 2, 2)
+
+// k.add([
+// 	k.pos(120, 80),
+// 	k.sprite("bean"),
+// ])
+
+// k.onClick(() => k.addKaboom(k.mousePos()))
+
+const k = kaboom({
+	scale: 4,
+	background: [0, 0, 0],
+})
+
+k.loadSpriteAtlas("sprites/001.png", {
+	bul: {
+		x: 0,
+		y: 0,
+		width: 256,
+		height: 256,
+		sliceX: 4,
+		sliceY: 4,
+		anims: {
+			idle: {
+				from: 0,
+				to: 3,
+				speed: 3,
+				loop: true
+			},
+			run: {
+				from: 8,
+				to: 11,
+				speed: 10,
+				loop: true
+			}
+		}
 	}
-)
+})
 
-k.loadSprite("bean", "sprites/bean.png")
+// https://0x72.itch.io/dungeontileset-ii
+k.loadSpriteAtlas("sprites/dungeon.png", {
+	hero: {
+		"x": 128,
+		"y": 196,
+		"width": 144,
+		"height": 28,
+		"sliceX": 9,
+		"anims": {
+			"idle": {
+				"from": 0,
+				"to": 3,
+				"speed": 3,
+				"loop": true,
+			},
+			"run": {
+				"from": 4,
+				"to": 7,
+				"speed": 10,
+				"loop": true,
+			},
+			"hit": 8,
+		},
+	},
+	"ogre": {
+		"x": 16,
+		"y": 388,
+		"width": 256,
+		"height": 30,
+		"sliceX": 8,
+		"anims": {
+			"idle": {
+				"from": 0,
+				"to": 3,
+				"speed": 3,
+				"loop": true,
+			},
+			"run": {
+				"from": 4,
+				"to": 7,
+				"speed": 10,
+				"loop": true,
+			},
+		},
+	},
+	"floor": {
+		"x": 16,
+		"y": 64,
+		"width": 48,
+		"height": 48,
+		"sliceX": 3,
+		"sliceY": 3,
+	},
+	"chest": {
+		"x": 304,
+		"y": 304,
+		"width": 48,
+		"height": 16,
+		"sliceX": 3,
+		"anims": {
+			"open": {
+				"from": 0,
+				"to": 2,
+				"speed": 20,
+				"loop": false,
+			},
+			"close": {
+				"from": 2,
+				"to": 0,
+				"speed": 20,
+				"loop": false,
+			},
+		},
+	},
+	"sword": {
+		"x": 322,
+		"y": 66,
+		"width": 12,
+		"height": 30,
+	},
+	"wall": {
+		"x": 16,
+		"y": 16,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_top": {
+		"x": 16,
+		"y": 0,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_left": {
+		"x": 16,
+		"y": 128,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_right": {
+		"x": 0,
+		"y": 128,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_topleft": {
+		"x": 32,
+		"y": 128,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_topright": {
+		"x": 48,
+		"y": 128,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_botleft": {
+		"x": 32,
+		"y": 144,
+		"width": 16,
+		"height": 16,
+	},
+	"wall_botright": {
+		"x": 48,
+		"y": 144,
+		"width": 16,
+		"height": 16,
+	},
+})
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
-])
+// Can also load from external JSON url
+// loadSpriteAtlas("/sprites/dungeon.png", "/sprites/dungeon.json")
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+// floor
+k.addLevel([
+	"xxxxxxxxxx",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+], {
+	tileWidth: 16,
+	tileHeight: 16,
+	tiles: {
+		" ": () => [
+			sprite("floor", { frame: ~~rand(0, 8) }),
+		],
+	},
+})
+
+// objects
+const map = addLevel([
+	"tttttttttt",
+	"cwwwwwwwwd",
+	"l        r",
+	"l        r",
+	"l        r",
+	"l      $ r",
+	"l        r",
+	"l $      r",
+	"attttttttb",
+	"wwwwwwwwww",
+], {
+	tileWidth: 16,
+	tileHeight: 16,
+	tiles: {
+		"$": () => [
+			sprite("chest"),
+			area(),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+			{ opened: false },
+			"chest",
+		],
+		"a": () => [
+			sprite("wall_botleft"),
+			area({ shape: new Rect(vec2(0), 4, 16) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"b": () => [
+			sprite("wall_botright"),
+			area({ shape: new Rect(vec2(12, 0), 4, 16) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"c": () => [
+			sprite("wall_topleft"),
+			area(),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"d": () => [
+			sprite("wall_topright"),
+			area(),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"w": () => [
+			sprite("wall"),
+			area(),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"t": () => [
+			sprite("wall_top"),
+			area({ shape: new Rect(vec2(0, 12), 16, 4) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"l": () => [
+			sprite("wall_left"),
+			area({ shape: new Rect(vec2(0), 4, 16) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+		"r": () => [
+			sprite("wall_right"),
+			area({ shape: new Rect(vec2(12, 0), 4, 16) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true }),
+		],
+	},
+})
+
+// const player = map.spawn([
+// 	sprite("hero", { anim: "idle" }),
+// 	area({ shape: new Rect(vec2(0, 6), 12, 12) }),
+// 	body(),
+// 	anchor("center"),
+// 	tile(),
+// ], 2, 2)
+
+const player = map.spawn([
+	sprite("bul"),
+	area({ shape: new Rect(vec2(0, 0), 32, 32) }),
+	body(),
+	scale(0.5, 0.5),
+	anchor("center"),
+	tile(),
+], 2, 2)
+
+// const sword = player.add([
+// 	pos(-4, 9),
+// 	sprite("sword"),
+// 	anchor("bot"),
+// 	rotate(0),
+// 	spin(),
+// ])
+
+// TODO: z
+map.spawn([
+	sprite("ogre", { anim: "idle" }),
+	anchor("bot"),
+	area({ scale: 0.5 }),
+	body({ isStatic: true }),
+	tile({ isObstacle: true }),
+], 5, 4)
+
+// function spin() {
+// 	let spinning = false
+// 	return {
+// 		id: "spin",
+// 		update() {
+// 			if (spinning) {
+// 				this.angle += 1200 * dt()
+// 				if (this.angle >= 360) {
+// 					this.angle = 0
+// 					spinning = false
+// 				}
+// 			}
+// 		},
+// 		spin() {
+// 			spinning = true
+// 		},
+// 	}
+// }
+
+function interact() {
+	const pointerPosition = k.mousePos();
+	const pointerPositionX = k.mousePos().x
+	const pointerPositionY = k.mousePos().y
+
+	debug.log('player Pos: ' + player.pos + ' MousePos: ' + pointerPosition)
+
+	// let interacted = false
+	// player.moveTo(pointerPosition);
+	// debug.log(vector)
+
+	// player.move();
+
+	// for (const col of player.getCollisions()) {
+	// 	const c = col.target
+	// 	if (c.is("chest")) {
+	// 		if (c.opened) {
+	// 			c.play("close")
+	// 			c.opened = false
+	// 		} else {
+	// 			c.play("open")
+	// 			c.opened = true
+	// 		}
+	// 		interacted = true
+	// 	}
+	// }
+	// if (!interacted) {
+	// 	// sword.spin()
+	// }
+}
+
+onMouseMove((action) => {
+	debug.log('mousePos: ' + action + 'playerPos: ' + player.pos);
+})
+
+
+
+onKeyPress("space", interact)
+
+const SPEED = 120
+
+const dirs = {
+	"left": LEFT,
+	"right": RIGHT,
+	"up": UP,
+	"down": DOWN,
+}
+
+player.onUpdate(() => {
+	camPos(player.pos)
+})
+
+player.onPhysicsResolve(() => {
+	// Set the viewport center to player.pos
+	camPos(player.pos)
+})
+
+onKeyDown("right", () => {
+	player.flipX = false
+	// sword.flipX = false
+	player.move(SPEED, 0)
+	// sword.pos = vec2(-4, 9)
+})
+
+onKeyDown("left", () => {
+	player.flipX = true
+	// sword.flipX = true
+	player.move(-SPEED, 0)
+	// sword.pos = vec2(4, 9)
+})
+
+onKeyDown("up", () => {
+	player.move(0, -SPEED)
+})
+
+onKeyDown("down", () => {
+	player.move(0, SPEED)
+})
+
+onGamepadButtonPress("south", interact)
+
+onGamepadStick("left", (v) => {
+	if (v.x < 0) {
+		player.flipX = true
+		// sword.flipX = true
+		// sword.pos = vec2(4, 9)
+	} else if (v.x > 0) {
+		player.flipX = false
+		// sword.flipX = false
+		// sword.pos = vec2(-4, 9)
+	}
+	player.move(v.scale(SPEED))
+	if (v.isZero()) {
+		if (player.curAnim() !== "idle") player.play("idle")
+	} else {
+		if (player.curAnim() !== "run") player.play("run")
+	}
+});
+
+["left", "right", "up", "down"].forEach((key) => {
+	onKeyPress(key, () => {
+		player.play("run")
+	})
+	onKeyRelease(key, () => {
+		if (
+			!isKeyDown("left")
+			&& !isKeyDown("right")
+			&& !isKeyDown("up")
+			&& !isKeyDown("down")
+		) {
+			player.play("idle")
+		}
+	})
+})
+debug.inspect = true
